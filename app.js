e7b9f3d13dbfd22a70a7dcdf18a442ce6850e3a8
Timo Mechsner <timo.mechsner@mailbox.org>
2016-08-13 19:49:00 +0000

    Changed to hello jenkins

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello jenkins');
 });
  
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8000);
  
 module.exports = app;
